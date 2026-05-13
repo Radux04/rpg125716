5c1d9abd67b8909f506b2afa27f1fbcfa7c84e9f
Radux04 <raducu.alessandro@gmail.com>
2026-05-13 17:28:22 +0200

    Refactor Item interface to abstract class and implement Inventory management system with ItemType enumeration

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125716/model/item/Inventory.java
@@ -0,0 +1,51 @@
+package it.unicam.cs.mpgc.rpg125716.model.item;
+
+import it.unicam.cs.mpgc.rpg125716.model.character.Player;
+import lombok.ToString;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ToString
+public class Inventory {
+    private final Map<Item, Integer> items = new LinkedHashMap<>();
+
+    public void addItem(Item item) {
+        items.merge(item, 1, Integer::sum);
+    }
+
+    public boolean removeItem(Item item) {
+        Integer quantity = items.get(item);
+
+        if (quantity == null) {
+            return false;
+        }
+
+        if (quantity == 1) {
+            items.remove(item);
+        } else {
+            items.put(item, quantity - 1);
+        }
+
+        return true;
+    }
+
+    public boolean useItem(Player player, Item item) {
+        if (!items.containsKey(item)) {
+            return false;
+        }
+
+        item.use(player);
+
+        if (item.isConsumable()) {
+            removeItem(item);
+        }
+
+        return true;
+    }
+
+    public Map<Item, Integer> getItems() {
+        return Collections.unmodifiableMap(items);
+    }
+}
--- src/main/java/it/unicam/cs/mpgc/rpg125716/model/item/Item.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125716/model/item/Item.java
@@ -1,9 +1,25 @@
 package it.unicam.cs.mpgc.rpg125716.model.item;
 
 import it.unicam.cs.mpgc.rpg125716.model.character.Player;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
-public interface Item {
-    String getName();
+@Getter
+@EqualsAndHashCode
+@ToString
+public abstract class Item {
+    private final String name;
+    private final ItemType type;
+    private final String description;
+    private final boolean consumable;
 
-    void use(Player player);
+    protected Item(String name, ItemType type, String description, boolean consumable) {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.consumable = consumable;
+    }
+
+    public abstract void use(Player player);
 }
